c464b8b8fd50d917d55794a475ebbeba1596cea9
taliforsyth <avfo2069@colorado.edu>
2022-10-12 15:52:22 -0600

    ICA8

---
+++ ICA8/ica8.js
@@ -0,0 +1,17 @@
+
+
+document.getElementsById('changeContent').style.fontSize='35px'
+
+let body = document.querySelector('h3, h4');
+body.addEventListener('click', changeColor);
+
+function changeColor(){
+  document.body.style.backgroundColor = 'PaleVioletRed';
+  document.body.style.color = 'white';
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  document.body.style.backgroundColor = 'PaleVioletRed';
+  document.body.style.color = 'white;
+)}
